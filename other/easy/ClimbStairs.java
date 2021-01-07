120f81bc03c95d251e1007e0cf53baa33b111ede
yclimb <yclimb@qq.com>
2021-01-07 16:44:30 +0800

    70. 爬楼梯

---
+++ other/easy/ClimbStairs.java
@@ -0,0 +1,102 @@
+package other.easy;
+
+/**
+ * 70. 爬楼梯
+ *
+ * @author yclimb
+ * @date 2021/1/7
+ */
+public class ClimbStairs {
+    public static void main(String[] args) {
+        test();
+    }
+
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     *
+     * 注意：给定 n 是一个正整数。
+     *
+     * 示例 1：
+     *
+     * 输入： 2
+     * 输出： 2
+     * 解释： 有两种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶
+     * 2.  2 阶
+     * 示例 2：
+     *
+     * 输入： 3
+     * 输出： 3
+     * 解释： 有三种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶 + 1 阶
+     * 2.  1 阶 + 2 阶
+     * 3.  2 阶 + 1 阶
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/climbing-stairs
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    private static void test() {
+        int n = 5;
+        int r1 = climbStairs(n);
+        int r2 = climbStairs2(n);
+        int r3 = climbStairs3(n);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+    }
+
+    /**
+     * 递归
+     */
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    /**
+     * 动态规划
+     */
+    public static int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i -1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    /**
+     * 滚动数组
+     */
+    public static int climbStairs3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
